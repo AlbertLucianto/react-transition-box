5863e28f05094909fa649be86b8ae9bf4ab6d144
AlbertLucianto <albertlucianto@gmail.com>
2019-03-13 02:22:14 +0800

    [TransitioningContainer] implement

--- src/TransitioningContainer.tsx
+++ src/TransitioningContainer.tsx
@@ -1,1 +1,35 @@
-export default class A {};
+import React, {
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import './style.scss';
+
+interface ITransitioningContainerProps {
+  children: JSX.Element|string;
+}
+
+function TransitioningContainer({ children }: ITransitioningContainerProps) {
+  const ref: React.Ref<HTMLDivElement> = useRef(null);
+  const [style, setStyle] = useState(null);
+
+  useEffect(() => {
+    const onResizeSetStyle = (e: UIEvent) => {
+      const { width, height } = (e.target as HTMLDivElement).getBoundingClientRect();
+      setStyle({ width, height });
+    };
+
+    ref.current.addEventListener('resize', onResizeSetStyle);
+    return ref.current.removeEventListener('resize', onResizeSetStyle);
+  }, [ref]);
+
+  return (
+    <div style={style}>
+      <div ref={ref} className="transitioning-container__inner">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default TransitioningContainer;
